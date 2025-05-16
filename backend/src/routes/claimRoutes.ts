26d2877a1e10ed6773fc7dd97ffdb75e0ef4c384
Sahil <sahil2215ali@gmail.com>
2025-05-16 17:12:33 +0530

    refactor

--- backend/src/routes/claimRoutes.ts
+++
@@ -1,9 +0,0 @@
-// // backend/src/routes/claimRoutes.ts
-// import express, { Router } from 'express';
-// import { claimTokens } from '../controllers/claimController';
-
-// const router: Router = express.Router();
-
-// router.post('/:claimId', claimTokens);
-
-// export default router;
--- backend/src/routes/purchaseRoutes.ts
+++
@@ -1,9 +0,0 @@
-// // backend/src/routes/purchaseRoutes.ts
-// import express, { Router } from 'express';
-// import { handleCryptoPurchase } from '../controllers/purchaseController';
-
-// const router: Router = express.Router();
-
-// router.post('/crypto', handleCryptoPurchase);
-
-// export default router;
